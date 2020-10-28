83a4ad889f131f2b58a9eb90e9b2d6046561fa7f
Federico <federico.gomez.sa@gmail.com>
2020-10-28 11:35:55 -0600

    Initial commit Be Go project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>Iniciando maqueta BEGO</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
